f2a5f30e4862f3f63863e2852d2dd04a46791c26
Joseph Turchiano <jturchia@github.com>
2013-04-11 03:03:03 -0400

    Basic Card-Flip Code
    Currently working with hardcoded images. Soon as we either get card/deck
    returns in the test API or I can figure out how to do it in deckops I
    can implement with ajax for any card.

--- code.js
+++ code.js
@@ -1,4 +1,16 @@
-/*$( document ).ready(function() {
+$( document ).ready(function() {
 		
-	$("form").submit(function(e) {	});
-});*/
+		//Change image from front to back. Will eventually implement ajax for generic cards
+		$("img").click(function(event) {
+			if ( $("img").attr("src") == "http://www.flashyapp.com/user_images/1.jpg")
+			{
+				event.preventDefault();
+				$("img").attr("src", "http://www.flashyapp.com/user_images/4.gif");
+			}
+			else if ( $("img").attr("src") == "http://www.flashyapp.com/user_images/4.gif")
+			{
+				event.preventDefault();
+				$("img").attr("src", "http://www.flashyapp.com/user_images/1.jpg");
+			}
+		});
+});
