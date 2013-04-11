e598e0d59223726bd7cd2cca815bb34c7f46ee8b
Joseph Turchiano <jturchia@github.com>
2013-04-11 17:24:35 -0400

    (Very) Basic Infrastructure
    Added temporary infrastructure for ease of access to all pages, and
    replaced the placeholder images in getcard.html with less awesome ones.

--- code.js
+++ code.js
@@ -2,15 +2,15 @@ $( document ).ready(function() {
 		
 		//Change image from front to back. Will eventually implement ajax for generic cards
 		$("img").click(function(event) {
-			if ( $("img").attr("src") == "http://www.flashyapp.com/user_images/1.jpg")
+			if ( $("img").attr("src") == "http://www.flashyapp.com/user_images/11.png")
 			{
 				event.preventDefault();
-				$("img").attr("src", "http://www.flashyapp.com/user_images/4.gif");
+				$("img").attr("src", "http://www.flashyapp.com/user_images/12.png");
 			}
-			else if ( $("img").attr("src") == "http://www.flashyapp.com/user_images/4.gif")
+			else if ( $("img").attr("src") == "http://www.flashyapp.com/user_images/12.png")
 			{
 				event.preventDefault();
-				$("img").attr("src", "http://www.flashyapp.com/user_images/1.jpg");
+				$("img").attr("src", "http://www.flashyapp.com/user_images/11.png");
 			}
 		});
 });
