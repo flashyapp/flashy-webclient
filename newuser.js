2fffef54e28bbad484de012badfeb25a05dfa3c6
Joseph Turchiano <jturchia@github.com>
2013-04-20 04:01:22 -0400

    Non-working Ajax post
    Problematic. See email.
    
    Working on fix.

--- newuser.js
+++ newuser.js
@@ -3,14 +3,26 @@ $( document ).ready(function() {
 		//AJAX - send new user data to server
 		$("#form").submit(function(event) {
 			event.preventDefault();
-			var userinfo = $("#form").serializeObject();
+			var request = $("#form").serializeObject();
 			$.ajax({
 				url: "http://www.flashyapp.com/api/user/create_user",
-				data: userinfo,
+				data: request,
+				contentType: "application/json",
 				type: "POST",
 				dataType: "json",
-				success: function(json) { alert("New user created."); },
-				error: function(xhr, status) { alert("Unknown error."); },
+				success: function(json) {
+					if (json.username_s == 0) alert("Error handling username status.");
+					if (json.password_s == 0) alert("Error handling password status");
+					if (json.email_s == 0) alert("Error handling email status");
+					alert("New user created."); },
+				error: function(xhr, status) { 
+					//var json = $.parseJSON(xhr.responseText);
+					console.log(request);
+					if (json.username_s == 0) alert("Invalid username.");
+					if (json.password_s == 0) alert("Invalid password.");
+					if (json.email_s == 0) alert("Invalid email address.");
+					if ((json.username_s == 1) && (json.password_s == 1) 
+						&& (json.email_s == 1)) alert("Unknown error."); },
 				complete: function(xhr, status) { ; }
 				});
 		});
