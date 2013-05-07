6b38fe64bc5eb9a6de7d7728304e043a4b61e280
Joseph Turchiano <jturchia@github.com>
2013-05-06 20:28:16 -0400

    Moving Lines Generalized
    upload_image:
    
    The uploaded image now pops up in a JQuery UI dialog box. Movable lines
    now show up in the box as well. The lines and the dialog box are
    tailored to fit the size of the image.
    
    Next step: lines should actually reflect the divs I receive from the
    server.

--- createdeck.js
+++ createdeck.js
@@ -33,7 +33,7 @@ $( document ).ready(function() {
 				if (json.error == 500) alert("Error: You don't have the permissions to access this feature. Please login or create a user account.");
 				if (json.error == 101) alert("Error: You don't have the permissions to access this feature. Please login or create a user account.");
 				if (json.error == 200) alert("Error: Please upload a valid image");
-				if (json.error == 0) alert("Success!");
+				if (json.error == 0) displayImage(json.divs, json.name);
 				console.log(json);
 			},
 			error: function(xhr, status) { 
@@ -90,6 +90,183 @@ $( document ).ready(function() {
 		
 		
 	});
+	
+//
+function displayImage(divs, name)
+{
+	$("#divlines").append("<img src='http://www.flashyapp.com/resources/tmp/" + name 
+		+ "' alt='preview' class='image' id='image'></img>");
+	
+	var w;
+	var h;
+	
+	//adapted from http://stackoverflow.com/questions/623172/how-to-get-image-size-height-width-using-javascript
+	var img = new Image();
+	img.onload = function() {
+		w = this.width;
+		h = this.height;
+		$("#image").attr("width", w);
+		$("#image").attr("height", h);
+		console.log(this.width + "x" + this.height);
+		drawImageLines();
+		$("#divlines").dialog("option", "minHeight", h + 150);
+		$("#divlines").dialog("option", "minWidth", w + 150);
+	}	
+	
+	img.src = "http://www.flashyapp.com/resources/tmp/" + name;
+	
+	$("#divlines").dialog({
+		buttons: [ { text: "Cancel", click: function() { $(this).dialog("close"); } } ],
+		modal: true,
+		title: "Adjust line splitting",
+		close: function() { ; }
+	});
+	
+}
+
+
+
+function drawImageLines() {
+
+	var rowIndex = 0;
+	var colIndex = 0;
+	
+	window.numRows = 5;
+	window.numCols = 5;
+	
+	var x1 = parseInt($(".image").css("left"));
+	var y1 = parseInt($(".image").css("top"));
+	var x2 = parseInt($("#image").attr("width")) + parseInt($(".image").css("left"));
+	var y2 = parseInt($("#image").attr("height")) + parseInt($(".image").css("top"));
+
+	console.log(x1 + " " + y1 + " " + x2 + " " + y2); 
+		
+	drawImageBox(x1, y1, x2, y2);
+	var boxHeight = Math.abs(y1 - y2);
+	var boxWidth = Math.abs(x1 - x2);
+	
+	var prevY = y1;
+	for (i = 0.0; i < numRows - 1; i++)
+	{
+		var y = (i+1) * y2 / numRows;	
+		var yid = "row" + rowIndex;
+		drawHorizontalLine(x1, y, x2, y, rowIndex);
+		
+		colIndex = 0;
+		for (j = 0.0; j < numCols - 1; j++)
+		{
+			var x = (j+1) * x2 / numCols;
+			var xid = yid + "col" + colIndex;
+			drawVerticalLine(x, prevY, x, y, xid);
+			colIndex++;
+		}
+		
+		prevY = y;
+		rowIndex++;
+	}
+	
+	//bottom row
+	colIndex = 0;
+	for (j = 0.0; j < numCols - 1; j++)
+	{
+		var x = (j+1) * x2 / numCols;
+		var xid = "row" + rowIndex + "col" + colIndex;
+		drawVerticalLine(x, prevY, x, y2, xid);
+		colIndex++;
+	}
+}
+
+
+function drawImageBox(x1, y1, x2, y2, imageid) {
+
+	var width = Math.abs(x1 - x2);
+	var height = Math.abs(y1 - y2);
+	var box = "<div id='box' class='box'> </div>";
+	
+	$("#divlines").append(box);
+	
+	$("#box").css({
+		left: x1,
+		top: y1,
+		width: width,
+		height: height,
+		position: 'absolute'
+	});
+}
+
+//draw a horizontal line - adapted from http://stackoverflow.com/questions/3157089/javascript-draw-line-with-jquery
+function drawHorizontalLine(x1, y1, x2, y2, index) {
+
+	var width = Math.abs(x1 - x2);
+	var id = "row" + index;
+	var line = "<div id='" + id + "' class='line'> </div>";
+	
+	$("#divlines").append(line);
+	
+	$("#"+ id).css({
+		left: x1,
+		top: y1,
+		width: width,
+		height: 0,
+		position: 'absolute',		
+		backgroundColor: '#000'
+	});
+	
+	$("#"+ id).draggable({
+		axis: "y",
+		containment: '#box',
+		drag: function() {
+			//change length of vertical lines
+			for (i = 0; i < window.numCols - 1; i++)
+			{
+				//top row
+				if (index == 0) {
+					$("#row" + index + "col" + i).css("height", parseInt($("#" + id).css("top")) - parseInt($("#box").css("top")));
+					$("#row" + (index + 1) + "col" + i).css("top", $("#" + id).css("top"));
+					$("#row" + (index + 1) + "col" + i).css("height", parseInt($("#row" + (index + 1)).css("top")) - parseInt($("#" + id).css("top"))); 
+				}
+				//bottom row
+				else if (index == window.numRows - 2) {
+					
+					$("#row" + index + "col" + i).css("height", parseInt($("#" + id).css("top")) - parseInt($("#row" + (index - 1)).css("top")));
+					$("#row" + (index + 1) + "col" + i).css("top", $("#" + id).css("top"));
+					$("#row" + (index + 1) + "col" + i).css("height", parseInt($("#box").css("top")) + parseInt($("#box").css("height")) - parseInt($("#" + id).css("top"))); 
+				}
+				//everything else
+				else {
+					$("#row" + index + "col" + i).css("height", parseInt($("#" + id).css("top")) - parseInt($("#row" + (index - 1)).css("top")));
+					$("#row" + (index + 1) + "col" + i).css("top", $("#" + id).css("top"));
+					$("#row" + (index + 1) + "col" + i).css("height", parseInt($("#row" + (index + 1)).css("top")) - parseInt($("#" + id).css("top"))); 
+					//console.log($("#row" + index + "col" + i).css("height"));
+				}
+			}
+		}
+	});
+}
+
+//draw a vertical, draggable line
+function drawVerticalLine(x1, y1, x2, y2, xid) {
+
+	var height = Math.abs(y1 - y2);
+	var id = xid;
+	var line = "<div id='" + id + "' class='line'> </div>";
+	
+	$("#divlines").append(line);
+	
+	$("#"+ id).css({
+		left: x1,
+		top: y1,
+		width: 0,
+		height: height,
+		position: 'absolute',		
+		backgroundColor: '#000'
+	});
+	
+	$("#"+ id).draggable({
+		axis: "x",
+		containment: '#box'
+	});
+}
 
 //Transforms textarea list of terms into a deck of cards
 function createCards(string)
